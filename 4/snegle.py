86adb559f77544e42e9ca9b9913ad0731b22de56
Marius Sørensen <marius.sorensen@smartstudy.no>
2019-12-04 17:27:07 +0100

    4 init

---
+++ 4/snegle.py
@@ -0,0 +1,36 @@
+import numpy as np
+import csv
+
+m = np.zeros((1000, 1000))
+pos = [0,0]
+time = 0
+
+a = np.asarray(m)
+np.savetxt("foo.csv", a, delimiter=",")
+
+with open("coords.csv") as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+    for row in reader:
+        row = [int(row[0]), int(row[1])]
+        if row[0] < pos[0]:
+            for x in range(row[0], pos[0]):
+                m[x][pos[1]] += 1
+                time += m[x][pos[1]]
+        else:
+            for x in range(pos[0], row[0]):
+                m[x][pos[1]] += 1
+                time += m[x][pos[1]]
+        pos[0] = row[0]
+
+        if row[1] < pos[1]:
+            for y in range(row[1], pos[1]):
+                m[pos[0]][y] += 1
+                time += m[pos[0]][y]
+        else:
+            for y in range(pos[1], row[1]):
+                m[pos[0]][y] += 1
+                time += m[pos[0]][y]
+        pos[1] = row[1]
+
+print(time)
