5ffa37a09622e9acd74646cf17b0e4dc18747d07
sorensenmarius <44672083+sorensenmarius@users.noreply.github.com>
2019-12-06 14:08:50 +0100

    6

---
+++ 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bm = (Bitmap)System.Drawing.Image.FromFile("mush.png", true);
+            Bitmap tmp = new Bitmap(bm.Width, bm.Height);
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 1; x < bm.Width; x++)
+                {
+                    Color c1 = bm.GetPixel(x - 1, y);
+                    Color c2 = bm.GetPixel(x, y);
+                    tmp.SetPixel(x, y, Color.FromArgb(c1.R ^ c2.R, c1.G ^ c2.G, c1.G ^ c2.B));
+                }
+            }
+            tmp.Save("result.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
