917e9d588c85f11558bfcf7ee8443867903227e1
Marius Sørensen <marius.sorensen@smartstudy.no>
2019-12-08 14:30:04 +0100

    7

---
+++ 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (long i = 2; i <= 27644437; i++) {
+                long b = i * 7;
+                long r = b % 27644437;
+                if (r == 1)
+                { 
+                    long z = 5897 * i;
+                    long svar = z % 27644437;
+                    Console.WriteLine(svar);
+                    break;
+                }
+            }
+        }
+    }
+}
