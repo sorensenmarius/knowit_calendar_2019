dfd77861528db9167353f80853fb1623345863b2
Marius Sørensen <marius.sorensen@smartstudy.no>
2019-12-13 16:08:16 +0100

    13

---
+++ 13/Program.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ja, jeg vet at dette er trash.");
+            Console.WriteLine(Arthur() - Isaac());
+        }
+
+        static int Arthur()
+        {
+            var jsonText = File.ReadAllText("./MAZE.txt");
+            var rooms = JsonConvert.DeserializeObject<List<List<Room>>>(jsonText);
+            List<(int x, int y)> moves = new List<(int x, int y)>();
+            moves.Add((0, 0));
+            int x = 0;
+            int y = 0;
+            int counter = 0;
+            Boolean moved;
+            while (x != 499 || y != 499)
+            {
+                rooms[y][x].notvisited = false;
+                counter += 1;
+                moved = false;
+                //Aust
+                if (!rooms[y][x].aust && rooms[y][x + 1].notvisited)
+                {
+                    moves.Add((x + 1, y));
+                    moved = true;
+                }
+                //Syd
+                else if (!rooms[y][x].syd && rooms[y + 1][x].notvisited)
+                {
+                    moves.Add((x, y + 1));
+                    moved = true;
+                }
+                //Vest
+                else if (!rooms[y][x].vest && rooms[y][x - 1].notvisited)
+                {
+                    moves.Add((x - 1, y));
+                    moved = true;
+                }
+                //Nord
+                else if (!rooms[y][x].nord && rooms[y - 1][x].notvisited)
+                {
+                    moves.Add((x, y - 1));
+                    moved = true;
+                }
+                if (moved)
+                {
+                    x = moves[moves.Count - 1].x;
+                    y = moves[moves.Count - 1].y;
+                }
+                else
+                {
+                    x = moves[moves.Count - 2].x;
+                    y = moves[moves.Count - 2].y;
+                    moves.RemoveAt(moves.Count - 1);
+                    counter -= 1;
+                }
+            }
+            return counter;
+        }
+
+        static int Isaac()
+        {
+            var jsonText = File.ReadAllText("./MAZE.txt");
+            var rooms = JsonConvert.DeserializeObject<List<List<Room>>>(jsonText);
+            List<(int x, int y)> moves = new List<(int x, int y)>();
+            moves.Add((0, 0));
+            int x = 0;
+            int y = 0;
+            int counter = 0;
+            Boolean moved;
+            while (x != 499 || y != 499)
+            {
+                rooms[y][x].notvisited = false;
+                counter += 1;
+                moved = false;
+                //Syd
+                if (!rooms[y][x].syd && rooms[y + 1][x].notvisited)
+                {
+                    moves.Add((x, y + 1));
+                    moved = true;
+                }
+                //Aust
+                else if (!rooms[y][x].aust && rooms[y][x + 1].notvisited)
+                {
+                    moves.Add((x + 1, y));
+                    moved = true;
+                }
+                //Vest
+                else if (!rooms[y][x].vest && rooms[y][x - 1].notvisited)
+                {
+                    moves.Add((x - 1, y));
+                    moved = true;
+                }
+                //Nord
+                else if (!rooms[y][x].nord && rooms[y - 1][x].notvisited)
+                {
+                    moves.Add((x, y - 1));
+                    moved = true;
+                }
+                if (moved)
+                {
+                    x = moves[moves.Count - 1].x;
+                    y = moves[moves.Count - 1].y;
+                }
+                else
+                {
+                    x = moves[moves.Count - 2].x;
+                    y = moves[moves.Count - 2].y;
+                    moves.RemoveAt(moves.Count - 1);
+                    counter -= 1;
+                }
+            }
+            return counter;
+        }
+    }
+}
+
+
+//20399
---
+++ 13/Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13
+{
+    class Room
+    {
+        public int x { get; }
+        public int y { get; }
+        public Boolean nord { get; }
+        public Boolean aust { get; }
+        public Boolean syd { get; }
+        public Boolean vest { get; }
+        public Boolean notvisited { get; set; }
+
+        public Room(int x, int y, Boolean nord, Boolean aust, Boolean syd, Boolean vest)
+        {
+            this.x = x;
+            this.y = y;
+            this.nord = nord;
+            this.aust = aust;
+            this.syd = syd;
+            this.vest = vest;
+            notvisited = true;
+        }
+
+        override
+        public string ToString()
+        {
+            return $"({x}, {y}): ({nord}, {aust}, {syd}, {vest}) - {notvisited}";
+        }
+    }
+}
