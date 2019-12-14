4be3b6bf6ae3a52207b8db1eb51e4b137f2b3e6b
Marius Sørensen <marius.sorensen@smartstudy.no>
2019-12-14 15:34:10 +0100

    14

--- 14/Program.cs
+++ 14/Program.cs
@@ -7,10 +7,12 @@ namespace _14
     {
         static void Main(string[] args)
         {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             int[] alf = new int[] { 2, 3, 5, 7, 11 };
             List<int> seq = new List<int>();
             int i = 0;
-            while(seq.Count < 217532235)
+            int sum = 0;
+            while (seq.Count < 217532235)
             {
                 if(seq.Count == 0)
                 {
@@ -21,19 +23,17 @@ namespace _14
                     for(int x = 0; x < seq[i]; x++)
                     {
                         seq.Add(alf[i % 5]);
+                        if(alf[i % 5] == 7)
+                        {
+                            sum += 7;
+                        }
                     }
                 }
                 i++;
             }
-            int sum = 0;
-            foreach(int num in seq)
-            {
-                if(num == 7)
-                {
-                    sum += 7;
-                }
-            }
             Console.WriteLine(sum);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
         }
     }
 }
